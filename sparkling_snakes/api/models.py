57a9c64550cfa9dcc8a7d052120ec24c20ec1c24
Filip Plachecki <filip.plachecki@gmail.com>
2022-11-28 22:30:38 +0100

    Add n from Task description & cleanup

--- sparkling_snakes/api/models.py
+++ sparkling_snakes/api/models.py
@@ -2,6 +2,8 @@ from fastapi import HTTPException
 from pydantic import BaseModel
 from abc import ABC, abstractmethod
 
+from sparkling_snakes import consts
+
 
 class Verifiable(ABC):
     @abstractmethod
@@ -15,8 +17,8 @@ class Verifiable(ABC):
 
 class NewTask(BaseModel, Verifiable):
     URL: str
+    n: int
 
     def validate_data(self) -> None:
-        if not self.URL:
-            raise HTTPException(status_code=400,
-                                detail="No Task or Task with improper value(s) has been provided")
+        if not self.URL or not self.n or self.n < consts.MINIMUM_PROCESSOR_TASKS_ALLOWED:
+            raise HTTPException(status_code=400, detail="No Task or Task with improper value(s) has been provided")
